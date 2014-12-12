6043a2ea983f34dc72261d47f457d8a23ca01074
crazytaxi <michael.jin.1729@gmail.com>
2014-12-12 06:56:28 -0500

    offset the point of interest by .6 radians about the x axis

---
+++ predict_point.c
@@ -0,0 +1,9 @@
+void rotVec (float* t, bool isRed) {        //must set x axis negative if you start as red
+    float quat[4] = {cosf(.3), sinf(.3), 0, 0};     //.6 radian difference from now
+    if (isRed) {            //start as red sphere
+        quat[1] = -quat[1];
+    }
+	t[0] = ((1 - 2*quat[2]*quat[2] -2*quat[3]*quat[3])*t[0] + (2*quat[1]*quat[2] + 2*quat[0]*quat[3])*t[1] + (2*quat[1]*quat[3]-2*quat[0]*quat[2])*t[2]);
+    t[1] = ((2*quat[1]*quat[2] - 2*quat[0]*quat[3])*t[0] + (1 - 2*quat[1]*quat[1] - 2*quat[3]*quat[3])*t[1] + (2*quat[2]*quat[3] + 2*quat[0]*quat[1])*t[2]);
+    t[2] = ((2*quat[1]*quat[3] + 2*quat[0]*quat[2])*t[0] + (2*quat[2]*quat[3] - 2*quat[0]*quat[1])*t[1] + (1 - 2*quat[1]*quat[1] - 2*quat[2]*quat[2])*t[2]);
+}
