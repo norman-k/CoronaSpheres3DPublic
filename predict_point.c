5816544f2bd74a5eb63fa5ac59087de2a77a7ef8
crazytaxi <michael.jin.1729@gmail.com>
2014-12-12 07:11:35 -0500

    removed boolean argument

--- predict_point.c
+++ predict_point.c
@@ -2,9 +2,9 @@
 	Michael Jin - Predict future location with same magnitude from origin
 */
 
-void rotVec (float* t, bool isRed) {        //must set x axis negative if you start as red
+void rotVec (float* t) {        //must set x axis negative if you start as red
     float quat[4] = {cosf(.3), sinf(.3), 0, 0};     //.6 radian difference from now
-    if (isRed) {            //start as red sphere
+    if (t[1] < 0) {            //start as red sphere
         quat[1] = -quat[1];
     }
 	t[0] = ((1 - 2*quat[2]*quat[2] -2*quat[3]*quat[3])*t[0] + (2*quat[1]*quat[2] + 2*quat[0]*quat[3])*t[1] + (2*quat[1]*quat[3]-2*quat[0]*quat[2])*t[2]);
